9409be59b9eea699d1b5ef7824d2d6476fc9594e
Foqsz <contatovictorvinicius05@gmail.com>
2024-10-08 09:47:59 -0300

    Paginaçao com bd

--- DotNet8-LINQ/Context/AppDbContext.cs
+++ DotNet8-LINQ/Context/AppDbContext.cs
@@ -1,10 +1,12 @@
 ﻿using DotNet8_LINQ.Entities;
+using DotNet8_LINQ.FiltrarDados;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Funcionario = DotNet8_LINQ.Entities.Funcionario;
 
 namespace DotNet8_LINQ.Context;
 
@@ -13,7 +15,7 @@ public class AppDbContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         //define o provedor do BD e a string de conexão
-        optionsBuilder.UseSqlServer("Server=localhost; Initial Catalog=LinqDB; Integrated Security=True; Trusted_Connection=true; TrustServerCertificate=True");
+        optionsBuilder.UseSqlServer("Server=localhost; Initial Catalog=Northwind; Integrated Security=True; Trusted_Connection=true; TrustServerCertificate=True");
         
         //exibe as consultas SQL no console
         optionsBuilder.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information);
@@ -21,5 +23,6 @@ public class AppDbContext : DbContext
 
     //mapeia as entidades para as tabelas do BD
     public DbSet<Setor> Setores { get; set; }
+    public DbSet<Product> Products { get; set; }
     public DbSet<Funcionario> Funcionarios { get; set; }
 }
